2a2fe79418b4b1acdee2ae6a7441f4acd25bae85
DOSalgueroM <daviniasalguero@gmail.com>
2024-05-04 18:40:54 -0400

    table completed

--- components/TableDataCategories.tsx
+++ components/TableDataCategories.tsx
@@ -1,95 +1,232 @@
-// import React from "react";
-// import { User, Tooltip } from "@headlessui/react";
-// import { EditIcon } from "./EditIcon";
-// import { DeleteIcon } from "./DeleteIcon";
-// import { EyeIcon } from "./EyeIcon";
-// import { columns, users } from "./data";
-// import Chip from "@headlessui/react/dist/components/chip";
+function TableDataCategories() {
+  return (
 
-// const statusColorMap: Record<string, string> = {
-//   active: "bg-green-500",
-//   paused: "bg-red-500",
-//   vacation: "bg-yellow-500",
-// };
+      <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
+          <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+              <thead className="bg-gray-50">
+                  <tr>
+                      <th scope="col" className="px-16 py-4 font-medium text-gray-900">Name</th>
+                      <th scope="col" className="px-6 py-4 font-medium text-gray-900">State</th>
+                      <th scope="col" className="px-6 py-4 font-medium text-gray-900"></th>
+                  </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+                  <tr className="hover:bg-gray-50">
+                      <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
+                          <div className="relative h-10 w-10">
+                              
+                          </div>
+                          <div className="text-sm">
+                              <div className="font-medium text-gray-700">Categoria1</div>
+                          </div>
+                      </th>
+                      <td className="px-6 py-4">
+                          <span
+                              className="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600">
+                              <span className="h-1.5 w-1.5 rounded-full bg-green-600"></span>
+                              Active
+                          </span>
+                      </td>
+                      {/* <td className="px-6 py-4">
+                          <div className="flex gap-2">
+                              
+                              
+                          </div>
+                      </td> */}
+                      <td className="px-6 py-4">
+                          <div className="flex justify-end gap-4">
+                              <a x-data="{ tooltip: 'Delete' }" href="#">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                      stroke="currentColor" className="h-6 w-6" x-tooltip="tooltip">
+                                      <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                  </svg>
+                              </a>
+                              <a x-data="{ tooltip: 'Edite' }" href="#">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                      stroke="currentColor" className="h-6 w-6" x-tooltip="tooltip">
+                                      <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                  </svg>
+                              </a>
+                          </div>
+                      </td>
+                  </tr>
+                  <tr className="hover:bg-gray-50">
+                      <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
+                          <div className="relative h-10 w-10">
+                             
+                          </div>
+                          <div className="text-sm">
+                              <div className="font-medium text-gray-700">Categoria2</div>
+                          
+                          </div>
+                      </th>
+                      <td className="px-6 py-4">
+                          <span
+                              className="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600">
+                              <span className="h-1.5 w-1.5 rounded-full bg-green-600"></span>
+                              Active
+                          </span>
+                      </td>
+                      
+                      {/* <td className="px-6 py-4">
+                          <div className="flex gap-2">
+                              
+                          </div>
+                      </td> */}
+                      <td className="px-6 py-4">
+                          <div className="flex justify-end gap-4">
+                              <a x-data="{ tooltip: 'Delete' }" href="#">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                      stroke="currentColor" className="h-6 w-6" x-tooltip="tooltip">
+                                      <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                  </svg>
+                              </a>
+                              <a x-data="{ tooltip: 'Edite' }" href="#">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                      stroke="currentColor" className="h-6 w-6" x-tooltip="tooltip">
+                                      <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                  </svg>
+                              </a>
+                          </div>
+                      </td>
+                  </tr>
+                  <tr className="hover:bg-gray-50">
+                      <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
+                          <div className="relative h-10 w-10">
+                            
+                          </div>
+                          <div className="text-sm">
+                              <div className="font-medium text-gray-700">Categoria3</div>
+                              
+                          </div>
+                      </th>
+                      <td className="px-6 py-4">
+                          <span
+                              className="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600">
+                              <span className="h-1.5 w-1.5 rounded-full bg-green-600"></span>
+                              Active
+                          </span>
+                      </td>
+                      
+                      {/* <td className="px-6 py-4">
+                          <div className="flex gap-2">
+                              
+                          </div>
+                      </td> */}
+                      <td className="px-6 py-4">
+                          <div className="flex justify-end gap-4">
+                              <a x-data="{ tooltip: 'Delete' }" href="#">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                      stroke="currentColor" className="h-6 w-6" x-tooltip="tooltip">
+                                      <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                  </svg>
+                              </a>
+                              <a x-data="{ tooltip: 'Edite' }" href="#">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                      stroke="currentColor" className="h-6 w-6" x-tooltip="tooltip">
+                                      <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                  </svg>
+                              </a>
+                          </div>
+                      </td>
+                  </tr>
+                  <tr className="hover:bg-gray-50">
+                      <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
+                          <div className="relative h-10 w-10">
+                              
+                              
+                          </div>
+                          <div className="text-sm">
+                              <div className="font-medium text-gray-700">Categoria4</div>
+                              
+                          </div>
+                      </th>
+                      <td className="px-6 py-4">
+                          <span
+                              className="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600">
+                              <span className="h-1.5 w-1.5 rounded-full bg-green-600"></span>
+                              Active
+                          </span>
+                      </td>
+                      
+                      {/* <td className="px-6 py-4">
+                          <div className="flex gap-2">
+                              
+                          </div>
+                      </td> */}
+                      <td className="px-6 py-4">
+                          <div className="flex justify-end gap-4">
+                              <a x-data="{ tooltip: 'Delete' }" href="#">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                      stroke="currentColor" className="h-6 w-6" x-tooltip="tooltip">
+                                      <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                  </svg>
+                              </a>
+                              <a x-data="{ tooltip: 'Edite' }" href="#">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                      stroke="currentColor" className="h-6 w-6" x-tooltip="tooltip">
+                                      <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                  </svg>
+                              </a>
+                          </div>
+                      </td>
+                  </tr>
+                  <tr className="hover:bg-gray-50">
+                      <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
+                          <div className="relative h-10 w-10">
+                              
+                             
+                          </div>
+                          <div className="text-sm">
+                              <div className="font-medium text-gray-700">cateogria5</div>
+                              
+                          </div>
+                      </th>
+                      <td className="px-6 py-4">
+                          <span
+                              className="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600">
+                              <span className="h-1.5 w-1.5 rounded-full bg-green-600"></span>
+                              Active
+                          </span>
+                      </td>
+                      
+                      {/* <td className="px-6 py-4">
+                          <div className="flex gap-2">
+                              
+                          </div>
+                      </td> */}
+                      <td className="px-6 py-4">
+                          <div className="flex justify-end gap-4">
+                              <a x-data="{ tooltip: 'Delete' }" href="#">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                      stroke="currentColor" className="h-6 w-6" x-tooltip="tooltip">
+                                      <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                  </svg>
+                              </a>
+                              <a x-data="{ tooltip: 'Edite' }" href="#">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                      stroke="currentColor" className="h-6 w-6" x-tooltip="tooltip">
+                                      <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                  </svg>
+                              </a>
+                          </div>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
 
-// type User = typeof users[0];
+  );
+};
 
-// export default function TableDataCategories() {
-//   const renderCell = React.useCallback((user: User, columnKey: keyof User) => {
-//     const cellValue = user[columnKey];
-
-//     switch (columnKey) {
-//       case "name":
-//         return (
-//           <User className="flex items-center space-x-2">
-//             <img src={user.avatar} alt={user.name} className="w-8 h-8 rounded-full" />
-//             <div>
-//               <p className="font-medium">{cellValue}</p>
-//               <p className="text-sm text-gray-500">{user.email}</p>
-//             </div>
-//           </User>
-//         );
-
-//       case "status":
-//         return (
-//           <Chip className={`capitalize ${statusColorMap[cellValue]} text-white`} size="sm">
-//             {cellValue}
-//           </Chip>
-//         );
-
-//       case "actions":
-//         return (
-//           <div className="flex items-center space-x-2">
-//             <Tooltip content="Details">
-//               <span className="text-lg text-gray-400 cursor-pointer hover:text-gray-600">
-//                 <EyeIcon />
-//               </span>
-//             </Tooltip>
-//             <Tooltip content="Edit user">
-//               <span className="text-lg text-gray-400 cursor-pointer hover:text-gray-600">
-//                 <EditIcon />
-//               </span>
-//             </Tooltip>
-//             <Tooltip content="Delete user">
-//               <span className="text-lg text-red-500 cursor-pointer hover:text-red-600">
-//                 <DeleteIcon />
-//               </span>
-//             </Tooltip>
-//           </div>
-//         );
-
-//       default:
-//         return cellValue;
-//     }
-//   }, []);
-
-//   return (
-//     <div className="overflow-x-auto">
-//       <table className="min-w-full divide-y divide-gray-200">
-//         <thead className="bg-gray-50">
-//           <tr>
-//             {columns.map((column) => (
-//               <th
-//                 key={column.uid}
-//                 className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-//               >
-//                 {column.name}
-//               </th>
-//             ))}
-//           </tr>
-//         </thead>
-//         <tbody className="bg-white divide-y divide-gray-200">
-//           {users.map((item) => (
-//             <tr key={item.id} className="hover:bg-gray-50">
-//               {columns.map((column) => (
-//                 <td key={column.uid} className="px-6 py-4 whitespace-nowrap">
-//                   {renderCell(item, column.uid)}
-//                 </td>
-//               ))}
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
+export default TableDataCategories;
--- pages/admin/categories/index.tsx
+++ pages/admin/categories/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-//import TableDataCategories from "../../../components/TableDataCategories";
+import TableDataCategories from "../../../components/TableDataCategories";
 import { NextPage } from "next";
 
 const Categories: NextPage = () => {
@@ -7,14 +7,15 @@ const Categories: NextPage = () => {
     <>
       <p className="text-gray-700 text-3xl mb-16 font-bold">Categories</p>
 
-      <div className="grid lg:grid-cols-3 gap-5 mb-16">
         <div className="rounded bg-white h-40 shadow-sm">
-          {/* <TableDataCategories /> */}
+          
         </div>
 
+      
+      <div className="grid col-1 bg-white h-96 shadow-sm">
+      <TableDataCategories />
       </div>
-      <div className="grid col-1 bg-white h-96 shadow-sm"></div>
-      <h1>Hola mundo</h1>
+    
     </>
 
   );
